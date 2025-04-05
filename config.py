d26f28997e8d87b8b94bac4834c840201ef55122
arrman11 <eren320v@gmail.com>
2025-04-05 21:52:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or Fa
 WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
@@ -86,7 +86,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://hentaibot-q67a.onrender.com/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
