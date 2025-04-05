db68ee9f68e9f6c765009fc3716e6956a144a423
arrman11 <eren320v@gmail.com>
2025-04-05 23:26:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -70,10 +70,10 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "7e92a99228ab9aa73ac42f953328f33456
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/howuseme/7") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://kgnbotz.blogspot.com/2025/04/hentaihub.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
