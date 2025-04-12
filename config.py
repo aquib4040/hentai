40ccf79498891f51ae96bb9abf5e92ee6b3d333e
aquib4040 <mdaquibjawed1106@gmail.com>
2025-04-12 21:25:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,11 +62,11 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
 
 # Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "adrinolinks.in") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "1c5efdeff8d53d0070f287b37eda883cb7184d25") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "arolinks.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "e32725a33a1ba7ce1e349c531ea2182faf3e2e9d") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/howuseme/7") # how to open link 
 
 # Website Info:
